9dd28133537d1823fe5e3151afddc3e6cd6e56be
Antony <vontonnie@gmail.com>
2021-02-24 19:30:32 +0300

    Implement createIntent()

--- app/src/main/java/com/example/android/firebaseui_login_sample/AuthResultContract.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/AuthResultContract.kt
@@ -1,16 +1,47 @@
 package com.example.android.firebaseui_login_sample
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 
-class AuthResultContract: ActivityResultContract<Int, AuthUI.IdpConfig>() {
+class AuthResultContract : ActivityResultContract<Int, IdpResponse>() {
+
+    companion object {
+        const val INPUT_KEY = "input_key"
+    }
+
+    //create providers
+    private val providers =
+        listOf(AuthUI.IdpConfig.GoogleBuilder().build(), AuthUI.IdpConfig.EmailBuilder().build())
+
+
+
     override fun createIntent(context: Context, input: Int?): Intent {
-        TODO("Not yet implemented")
+        return AuthUI.getInstance().createSignInIntentBuilder()
+            .setLogo(R.drawable.ic_launcher_background)
+            .setTheme(R.style.AppTheme)
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(true)
+            .build().apply { putExtra(INPUT_KEY, input) }
     }
 
-    override fun parseResult(resultCode: Int, intent: Intent?): AuthUI.IdpConfig {
-        TODO("Not yet implemented")
+
+
+    override fun parseResult(resultCode: Int, intent: Intent?): IdpResponse? {
+
+        return when (resultCode ){
+
+            Activity.RESULT_OK -> IdpResponse.fromResultIntent(intent)
+            else -> null
+        }
+
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -46,7 +46,7 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
 
         // TODO Remove the two lines below once observeAuthenticationState is implemented.
